bb16140d52dd184250d7e58528266e1e79d2f0d0
Jhon Lucas <jhonlucaslife@gmail.com>
2025-07-06 18:26:08 -0300

    feat: add prometheus metrics.

--- src/streamers/kafka_consumer.py
+++ src/streamers/kafka_consumer.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import json
 from confluent_kafka import Consumer
 from src.utils.logger import get_logger
@@ -5,6 +6,7 @@ from src.utils.postgresql_client import PostgresClient
 from src.models.swap_events import SwapEvent
 from src.models.burn_events import BurnEvent
 from src.models.mint_events import MintEvent
+from prometheus_client import Counter, Gauge
 
 logger = get_logger(__name__)
 
@@ -28,6 +30,24 @@ class KafkaEventConsumer:
         self.kafka_topic = kafka_topic
         self.postgres_client = PostgresClient()
         
+        self.events_processed_counter = Counter(
+            name='events_processed_total',
+            documentation='Total number of events processed since startup',
+            labelnames=['event_name']
+        )
+
+        self.events_processed_latency_gauge = Gauge(
+            name='events_processed_latency',
+            documentation='Latency of events processed since block timestamp',
+            labelnames=['event_name']
+        )
+
+        self.last_processed_timestamp = Gauge(
+            name='last_event_processed_timestamp',
+            documentation='Timestamp of the last successfully processed event',
+            labelnames=['event_name']
+        )
+
         logger.info(f"KafkaEventConsumer initialized for topic: {self.kafka_topic}")
     
     def _handle_message(self, event_data: dict):
@@ -68,9 +88,19 @@ class KafkaEventConsumer:
                 
                 try:
                     event_data = json.loads(msg.value().decode('utf-8'))
+                    block_timestamp= event_data.get('decoded_event', {}).get('timestamp')
+                    block_timestamp = datetime.fromisoformat(block_timestamp).replace(tzinfo=timezone.utc)
+                    current_time = datetime.now(timezone.utc)
+                    latency = (current_time - block_timestamp).total_seconds()
                     decoded_event = event_data.get('decoded_event')
                     if decoded_event:
+                        self.events_processed_latency_gauge.labels(event_name=decoded_event.get('event_name', 'unknown')).set(latency)
+                        self.events_processed_counter.labels(event_name=decoded_event.get('event_name', 'unknown')).inc()
+                        self.last_processed_timestamp.labels(event_name=decoded_event.get('event_name', 'unknown')).set(block_timestamp.timestamp())
                         self._handle_message(decoded_event)
+                        logger.info(f"Event {decoded_event.get('event_name', 'unknown')} processed successfully")
+                    else:
+                        logger.error(f"Failed to decode event: {event_data}")
                     
                 except json.JSONDecodeError as e:
                     logger.error(f"Failed to decode message: {e}")

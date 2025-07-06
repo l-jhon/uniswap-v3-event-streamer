000449ba5b086e36b85eedc923186c320caaa26a
Jhon Lucas <jhonlucaslife@gmail.com>
2025-07-06 16:13:01 -0300

    chore: update init file.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .core import EventStreamer
 from .streamers import UniswapV3EventStreamer
+from .streamers import KafkaEventConsumer
 
-__all__ = ['EventStreamer', 'UniswapV3EventStreamer']
+__all__ = ['EventStreamer', 'UniswapV3EventStreamer', 'KafkaEventConsumer']
